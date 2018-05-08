e67e7e4d1f5f47a278c0e426a9934f9c7a987a95
Graham Neal <nealjg@gmail.com>
2018-05-08 08:21:24 -0400

    finally did the switch correctly

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('The American Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Grilled Pineapple Burger');
+INSERT INTO burgers (burger_name) VALUES ('Onion Jam Bacon Burger');
